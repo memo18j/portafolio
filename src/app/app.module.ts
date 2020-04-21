5f53552ba670d86481a083ade3343bf18fe007ee
Guillermo Parada <jguillermoparada@gmail.com>
2020-04-21 17:09:58 -0500

    leer json local

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,7 +20,7 @@ import { ItemComponent } from './pages/item/item.component';
     AboutComponent,
     ItemComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombrecorto?: string;
+  urlpagina?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  equipo?: any[];
+}
